2c028621c48ac512436b7003765c7bb2f1281282
Edward <edwardgretsch@gmail.com>
2018-08-14 16:56:19 -0700

    udate 6

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,5 @@ if (process.argv.JAWSDB_URL) {
   });
 }
 
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 // Export connection for our ORM to use.
 module.exports = connection;
