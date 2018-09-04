e1c5389bf1c1882d64dd47ac488f4c6a755454ef
Edward Gretsch <38367979+egretsch@users.noreply.github.com>
2018-09-04 01:37:46 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+
+// Provides us with the basic information to create our starter burgers
 INSERT INTO burgers (burger_name) VALUES ('Chuck Noris');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Chese burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester Staelone', true);
